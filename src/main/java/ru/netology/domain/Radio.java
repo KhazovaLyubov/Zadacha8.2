fda0d3e7c0b758fa2ed050a5b2f2184307428bd0
Khazova Lyubov <lyubashilina@rambler.ru>
2020-10-29 11:39:56 +0400

    Added changes in lombok branch

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -10,20 +10,11 @@ import lombok.NoArgsConstructor;
 class Radio {
     private int currentRadioStation;
     private int minRadioStation;
-    private int maxRadioStation = 10;
+    private int maxRadioStation;
     private int currentSoundVolume;
     private int minSoundVolume;
-    private int maxSoundVolume = 100;
+    private int maxSoundVolume;
 
-    public Radio(int currentRadioStation, int currentSoundVolume) {
-        this.currentRadioStation = currentRadioStation;
-        this.currentSoundVolume = currentSoundVolume;
-    }
-
-
-    public int getCurrentRadioStation() {
-        return currentRadioStation;
-    }
 
     public void setCurrentRadioStation(int currentRadioStation) {
         if (currentRadioStation > maxRadioStation) {
@@ -35,26 +26,6 @@ class Radio {
         this.currentRadioStation = currentRadioStation;
     }
 
-    public int getMinRadioStation() {
-        return minRadioStation;
-    }
-
-    public void setMinRadioStation(int minRadioStation) {
-        this.minRadioStation = minRadioStation;
-    }
-
-    public int getMaxRadioStation() {
-        return maxRadioStation;
-    }
-
-    public void setMaxRadioStation(int maxRadioStation) {
-        this.maxRadioStation = maxRadioStation;
-    }
-
-    public int getCurrentSoundVolume() {
-        return currentSoundVolume;
-    }
-
     public void setCurrentSoundVolume(int currentSoundVolume) {
         if (currentSoundVolume >= maxSoundVolume){
             this.currentSoundVolume = maxSoundVolume;
@@ -64,22 +35,6 @@ class Radio {
             return;
         }
         this.currentSoundVolume = currentSoundVolume;
-    }
-
-    public int getMinSoundVolume() {
-        return minSoundVolume;
-    }
-
-    public void setMinSoundVolume(int minSoundVolume) {
-        this.minSoundVolume = minSoundVolume;
-    }
-
-    public int getMaxSoundVolume() {
-        return maxSoundVolume;
-    }
-
-    public void setMaxSoundVolume(int maxSoundVolume) {
-        this.maxSoundVolume = maxSoundVolume;
     }
 
     public void onNextRadioStation() {
